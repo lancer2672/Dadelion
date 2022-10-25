4d7a8dbc4a4f7f5e04bec2df3e957f243238e61c
lancer <jbkhanhtran@gmail.com>
2022-10-25 09:46:22 +0700

    create Register component

--- app/src/components/Register.js
+++ app/src/components/Register.js
@@ -1,11 +1,13 @@
-import { View, Text,TextInput,Button, StyleSheet } from 'react-native'
+import { StyleSheet,TextInput,Button, Text, View } from 'react-native'
 import React from 'react'
 
-const Login = () => {
+const Register = () => {
   return (
     <View>
       <TextInput placeholder='Tên đăng nhập' ></TextInput>
       <TextInput placeholder='Mật khẩu'></TextInput>
+      <TextInput placeholder='Nhập lại mật khẩu'></TextInput>
+      <TextInput placeholder='Email'></TextInput>
       <Button
         title="Đăng nhập"
         color="#841584"
@@ -13,13 +15,9 @@ const Login = () => {
     </View>
   )
 }
+
+export default Register
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: "100%",
-    }
+
 })
-export default Login
