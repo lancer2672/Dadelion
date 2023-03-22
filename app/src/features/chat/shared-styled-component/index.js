d4e2fa007696ecc096bb74fb93e997e78000f649
lancer <jbkhanhtran@gmail.com>
2023-03-22 22:39:19 +0700

    asd

--- app/src/features/chat/shared-styled-component/index.js
+++
@@ -1,8 +0,0 @@
-import { Image } from "react-native";
-import styled from "styled-components/native";
-
-export const Avatar = styled(Image)`
-  border-radius: 25px;
-  width: 50px;
-  height: 50px;
-`;
--- app/src/utils/slogan.js
+++
@@ -1,3 +0,0 @@
-const AppSlogan = "First, you love, then you live.";
-
-export { AppSlogan };
