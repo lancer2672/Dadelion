7378baf421bfb70f5f853387022f8f098fab9c34
lancer2672 <jbkhanhtran@gmail.com>
2023-10-17 10:04:57 +0700

    remove unused colors

--- app/src/infrastructure/theme/colors.js
+++ app/src/infrastructure/theme/colors.js
@@ -1,34 +1,21 @@
 const palette = {
-  primary: "#4D5DFA",
+  primary: "#e3c3de",
   dark: "#1C244F",
   white: "#FFFFFF",
   light: "#E9ECFF",
   grayLight: "#90A3BC",
   green: "#3CB878",
   red: "#FF453A",
-  yellow: "#FFC12D",
   black1: "#252525",
-  black2: "#4C4F51",
-  black3: "#818283",
-  gray1: "#BFBFBF",
-  gray2: "#E1E0DF",
 };
+
 export const colors = {
-  ui: {
-    primary: "rgba(54, 100, 186,0.4)",
-    secondary: "#a1e6f3",
-    disabled: "#DEDEDE",
-    error: "#f03030",
-    success: "#138000",
-  },
   bg: {
     primary: "#f6f9ff",
     secondary: "#b3afab",
   },
   text: {
     primary: "#e3c3de",
-    secondary: "#d649c0",
-    disabled: "#9C9C9C",
     error: "#e35d5d",
     success: "#138000",
   },
@@ -38,8 +25,5 @@ export const colors = {
       secondary: "#f6f9ff",
     },
     text: "#000000",
-    icon: "#ecdfed",
   },
-  white: "#FFFFFF",
-  black: "#000000",
 };
