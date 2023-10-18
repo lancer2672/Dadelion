c4d953f2aa87c6a37e6caa5f99ac0c6566ac1b81
lancer2672 <jbkhanhtran@gmail.com>
2023-10-18 19:16:59 +0700

    remove unused code

--- app/src/utils/imageHelper.js
+++ app/src/utils/imageHelper.js
@@ -1,28 +1,4 @@
-import * as ImagePicker from "expo-image-picker";
 import * as FileSystem from "expo-file-system";
-
-// import { launchImageLibrary } from "react-native-image-picker";
-
-export const openImagePicker = () => {
-  // return new Promise((resolve, reject) => {
-  //   launchImageLibrary(
-  //     {
-  //       mediaType: "photo",
-  //       includeBase64: true,
-  //     },
-  //     (response) => {
-  //       if (response.didCancel) {
-  //         reject("User cancelled image picker");
-  //       } else if (response.error) {
-  //         reject("ImagePicker Error: ", response.error);
-  //       } else {
-  //         resolve(response);
-  //       }
-  //     }
-  //   );
-  // });
-};
-
 export const readBase64 = async (uri) => {
   try {
     const base64String = await FileSystem.readAsStringAsync(uri, {
